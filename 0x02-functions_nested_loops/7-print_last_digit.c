01e45a4667b30d5697758e1b3110d7425e4def7a
Aaron Obeng-Kyei <obengkyeiaaron@gmail.com>
2022-07-05 19:54:51 -0700

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,15 +10,17 @@
  */
 int print_last_digit(int n)
 {
-	int a;
+	int l;
 
-	if (n < 0)
-		n = -n;
-		a = n % 10;
-	if (a < 0)
-		a = -a;
-	_putchar(a + '0');
-
-	return (a);
-
+	l = n % 10;
+	if (l < 0)
+		{
+		_putchar(-l + 48);
+	return (-l);	
+		}
+	else
+	{
+	_putchar(l + 48);
+	return (l);
+	}	
 }
